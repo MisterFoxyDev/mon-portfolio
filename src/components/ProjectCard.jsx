20cdcd08e56786f1027b8e034351e6d8a9febc60
MisterFoxyDev <misterfoxydev@gmail.com>
2024-10-08 20:23:09 +0200

    modification taille des ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -46,79 +46,83 @@ const ProjectCard = ({
       className="relative m-4 h-fit w-80 max-w-sm transform overflow-hidden rounded-lg border-2 border-zinc-600 shadow-[4px_4px_15px_rgba(0,0,0,0.7)] transition-transform duration-300 hover:shadow-[10px_10px_20px_rgba(9,9,9,0.6)] dark:border-zinc-300 dark:shadow-none dark:shadow-gray-800/60"
     >
       <div className="pointer-events-none absolute left-0 top-0 h-full w-full animate-gradient-bg bg-gradient-to-r from-violet-800 via-blue-800 to-violet-600 bg-[length:400%_400%] opacity-60 dark:opacity-80"></div>
-      <div className="relative z-10 p-4">
-        <div className="relative">
-          <Image
-            src={staticImageUrl}
-            alt={title}
-            width={640}
-            height={640}
-            loading="lazy"
-            className={`mb-2 h-48 w-full rounded-lg object-fill ${
-              animatedImageUrl ? "hover:opacity-0" : ""
-            }`}
-          />
-          {animatedImageUrl && (
+      <div className="relative z-10 flex min-h-[500px] flex-col justify-between p-4">
+        <div>
+          <div className="relative">
             <Image
-              src={animatedImageUrl}
+              src={staticImageUrl}
               alt={title}
               width={640}
               height={640}
-              className="absolute left-0 top-0 mb-2 h-48 w-full rounded-lg object-fill opacity-0 hover:opacity-100"
-              unoptimized
+              loading="lazy"
+              className={`mb-2 h-48 w-full rounded-lg object-fill ${
+                animatedImageUrl ? "hover:opacity-0" : ""
+              }`}
             />
-          )}
-        </div>
-        <h1 className="font-serif text-xl font-bold text-zinc-900 dark:text-zinc-300">
-          {title}
-        </h1>
-        <p className="mb-2 text-zinc-800 dark:text-zinc-300">
-          {description.resume}
-        </p>
-        <p className="mb-2 text-sm text-zinc-900 dark:text-zinc-300">
-          <span className="text-lg font-semibold">
-            Technologies principales :
-          </span>{" "}
-          {technologies.principales.join(", ")}
-        </p>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="mb-2 font-medium text-blue-700 hover:underline focus:outline-none dark:text-blue-300"
-        >
-          {isOpen ? (
-            <>
-              Moins d&apos;infos <MdKeyboardArrowUp className="inline-block" />
-            </>
-          ) : (
-            <>
-              Plus d&apos;infos <MdKeyboardArrowDown className="inline-block" />
-            </>
-          )}
-        </button>
-        {isOpen && (
-          <div className="mt-2">
-            <p className="mb-2 text-base text-zinc-900 dark:text-zinc-300">
-              {description.complete}
-            </p>
-            {technologies.secondaires.length > 0 && (
-              <div>
-                <h3 className="text-base font-semibold text-zinc-900 dark:text-zinc-200">
-                  Technologies secondaires:
-                </h3>
-                <ul className="mb-4 list-inside list-disc">
-                  {technologies.secondaires.map((lib, index) => (
-                    <li
-                      key={index}
-                      className="text-zinc-900 dark:text-zinc-200"
-                    >
-                      {lib}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+            {animatedImageUrl && (
+              <Image
+                src={animatedImageUrl}
+                alt={title}
+                width={640}
+                height={640}
+                className="absolute left-0 top-0 mb-2 h-48 w-full rounded-lg object-fill opacity-0 hover:opacity-100"
+                unoptimized
+              />
             )}
           </div>
-        )}
+          <h1 className="font-serif text-xl font-bold text-zinc-900 dark:text-zinc-300">
+            {title}
+          </h1>
+          <p className="mb-2 text-zinc-800 dark:text-zinc-300">
+            {description.resume}
+          </p>
+          <p className="mb-2 text-sm text-zinc-900 dark:text-zinc-300">
+            <span className="text-lg font-semibold">
+              Technologies principales :
+            </span>{" "}
+            {technologies.principales.join(", ")}
+          </p>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="mb-2 font-medium text-blue-700 hover:underline focus:outline-none dark:text-blue-300"
+          >
+            {isOpen ? (
+              <>
+                Moins d&apos;infos{" "}
+                <MdKeyboardArrowUp className="inline-block" />
+              </>
+            ) : (
+              <>
+                Plus d&apos;infos{" "}
+                <MdKeyboardArrowDown className="inline-block" />
+              </>
+            )}
+          </button>
+          {isOpen && (
+            <div className="mt-2">
+              <p className="mb-2 text-base text-zinc-900 dark:text-zinc-300">
+                {description.complete}
+              </p>
+              {technologies.secondaires.length > 0 && (
+                <div>
+                  <h3 className="text-base font-semibold text-zinc-900 dark:text-zinc-200">
+                    Technologies secondaires:
+                  </h3>
+                  <ul className="mb-4 list-inside list-disc">
+                    {technologies.secondaires.map((lib, index) => (
+                      <li
+                        key={index}
+                        className="text-zinc-900 dark:text-zinc-200"
+                      >
+                        {lib}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
         <div className="flex justify-center">
           {siteUrl ? (
             <Link
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -2,19 +2,20 @@ import { BsAlignEnd } from "react-icons/bs";
 import { BsAlignStart } from "react-icons/bs";
 import ProjectCard from "@/components/ProjectCard";
 import { FaLaptopCode } from "react-icons/fa";
+import { memo } from "react";
 
-const ProjectsList = ({ projets }) => {
+const ProjectsList = memo(({ projets }) => {
   return (
     <section
       id="projets"
-      className="flex flex-col items-center overflow-x-hidden pb-4 pt-24 text-zinc-600 dark:text-zinc-300"
+      className="flex flex-col items-center overflow-x-hidden pt-24 text-zinc-600 dark:text-zinc-300"
     >
       <h1 className="mb-4 text-center font-serif text-2xl tracking-wider sm:text-3xl">
         {<BsAlignEnd className="inline-block text-sm sm:text-xl" />} Mes projets{" "}
         {<FaLaptopCode className="inline-block text-2xl" />}{" "}
         {<BsAlignStart className="inline-block text-sm sm:text-xl" />}
       </h1>
-      <div className="flex max-w-[80vw] flex-wrap justify-center gap-5 sm:max-w-[50dvw]">
+      <div className="flex max-w-[80vw] flex-wrap justify-center gap-5 pb-7 sm:max-w-[50dvw]">
         {projets.map((projet, index) => {
           const animationDirection = index % 2 === 0 ? "left" : "right";
           return (
@@ -33,6 +34,8 @@ const ProjectsList = ({ projets }) => {
       </div>
     </section>
   );
-};
+});
+
+ProjectsList.displayName = "ProjectsList";
 
 export default ProjectsList;
